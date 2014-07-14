f4745820f9c4b3586120faee85c186c896ce35ce
Diegoni <diego_nieto_1@hotmail.com>
2014-07-14 13:51:52 -0300

    1.0.1

--- application/views/frontend/formulario_reserva.php
+++ application/views/frontend/formulario_reserva.php
@@ -1,6 +1,50 @@
 <div class="container">
 <div class="row">
-	<div class="col-md-3 well">
-		asdf
+	<div class="col-md-3">
+		<div class="panel panel-default">
+	  		<div class="panel-heading">Reserva online</div>
+	  		<div class="panel-body">
+	    		<form role="form">
+					<div class="form-group">
+						<label for="exampleInputEmail1">Entrada</label>
+						<div class="input-group">
+							<div class="input-group-addon">
+								<span class="icon-calendarthree"></span>
+							</div>
+      						<input class="form-control" type="entrada" placeholder="Fecha entrada">
+    					</div>
+				  	</div>
+				  	<div class="form-group">
+					    <label for="exampleInputEmail1">Salida</label>
+						<div class="input-group">
+							<div class="input-group-addon">
+								<span class="icon-calendarthree"></span>
+							</div>
+      						<input class="form-control" type="salida" placeholder="Fecha salida">
+    					</div>
+					</div>
+					<div class="form-group">
+						<label for="exampleInputPassword1">Ocupación</label>
+						<select class="form-control">
+				  			<option>1 adulto</option>
+				  			<option>2 adultos</option>
+				  			<option>3 adultos</option>
+				  			<option>4 adultos</option>
+				  			<option>5 adultos</option>
+						</select>
+			     		<select class="form-control">
+				  			<option>1 menor</option>
+				  			<option>2 menores</option>
+				  			<option>3 menores</option>
+				  			<option>4 menores</option>
+				  			<option>5 menores</option>
+						</select>
+			  		</div>
+					<div class="form-group">
+				  		<button type="submit" class="btn btn-default">Ver precios</button>
+				  	</div>
+				</form>
+			</div>
+		</div>
 	</div>
 	
