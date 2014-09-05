066031f7791d7f5800c877b9106b03193a4ef5b1
Diegoni <diego_nieto_1@hotmail.com>
2014-09-05 15:19:30 -0300

    1.0.1

--- application/controllers/hoteles.php
+++ application/controllers/hoteles.php
@@ -1,6 +1,6 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Habitacion extends CI_Controller {
+class Hoteles extends CI_Controller {
 	
 	function __construct(){
 		parent::__construct();
@@ -14,7 +14,7 @@ class Habitacion extends CI_Controller {
 		$this->load->model('habitacion_servicio_model');
 		$this->load->model('tipos_habitacion_model');
 		$this->load->model('provincias_model');
-		$this->load->model('imagenes_habitacion_model');
+		$this->load->model('imagenes_hotel_model');
 		$this->load->model('monedas_model');
 		$this->load->helper('main');
 		$this->load->helper('form');
@@ -22,48 +22,54 @@ class Habitacion extends CI_Controller {
 	}
 	
 	
-	public function view($id=NULL){
-		if($id==NULL){
-			$id=$this->input->post('id');
-		}
-		$db['texto']=$this->idiomas_model->getIdioma();
-		$db['idiomas']=$this->idiomas_model->getIdiomas();
-		$db['hoteles']=$this->hoteles_model->getHoteles();
-		$db['habitaciones']=$this->habitaciones_model->getHabitacion($id);
-		$db['servicios']=$this->habitacion_servicio_model->getServicios($id);
-		$db['provincias']=$this->provincias_model->getProvincias('032');
-		$db['configs']=$this->configs_model->getConfigs();
-		$db['emails_hotel']=$this->hoteles_email_model->getEmails(2);
-								
-		$this->load->view('frontend/head', $db);
-		$this->load->view('frontend/menu');
-		$this->load->view('frontend/formulario_consulta');
-		$this->load->view('frontend/habitacion/view');
-		$this->load->view('frontend/footer');
-		
-	}
-	
 	public function galeria($id=NULL){
 		if($id==NULL){
 			$id=$this->input->post('id');
 		}
+		
 		$db['texto']=$this->idiomas_model->getIdioma();
 		$db['idiomas']=$this->idiomas_model->getIdiomas();
 		$db['hoteles']=$this->hoteles_model->getHoteles();
-		$db['habitaciones']=$this->habitaciones_model->getHabitacion($id);
-		$db['servicios']=$this->habitacion_servicio_model->getServicios($id);
-		$db['provincias']=$this->provincias_model->getProvincias('032');
+		$db['habitaciones']=$this->hoteles_model->getHotel($id);
 		$db['configs']=$this->configs_model->getConfigs();
 		$db['emails_hotel']=$this->hoteles_email_model->getEmails(2);
 								
 		$this->load->view('frontend/head', $db);
 		$this->load->view('frontend/menu');
-		$this->load->view('frontend/formulario_consulta');
-		$this->load->view('frontend/habitacion/galeria');
+		$this->load->view('frontend/formulario_reserva');
+		$this->load->view('frontend/hoteles/galeria');
 		$this->load->view('frontend/footer');
 		
 	}
 	
+	public function habitaciones(){
+		$db['texto']=$this->idiomas_model->getIdioma();
+		
+		$consulta=array('hotel'	=> 2);
+		
+		$db['hoteles']=$this->hoteles_model->getHoteles();
+		$db['hotel']=$this->hoteles_model->getHotel(2);
+		$db['habitaciones']=$this->habitaciones_model->getHabitaciones($consulta);
+		$db['idiomas']=$this->idiomas_model->getIdiomas();
+		$db['emails_hotel']=$this->hoteles_email_model->getEmails(2);
+		
+		if(!(isset($_COOKIE['moneda']))){
+			$_COOKIE['moneda']=1;
+		}
+		
+		$db['cambios']=$this->monedas_model->getMoneda($_COOKIE['moneda']);
+		$db['configs']=$this->configs_model->getConfigs();
+		$db['tipos_habitacion']=$this->tipos_habitacion_model->getTipos();
+		$db['tipo_habitacion']=$this->tipos_habitacion_model->getTipo($this->input->post('tipo'));
+		
+				
+		$this->load->view('frontend/head', $db);
+		$this->load->view('frontend/menu');
+		$this->load->view('frontend/formulario_reserva');
+		$this->load->view('frontend/hoteles/habitacion');
+		$this->load->view('frontend/footer');
+		
+	}
 	
 	
 	
---
+++ application/views/frontend/hoteles/galeria.php
@@ -0,0 +1,32 @@
+	<div class="col-md-9">
+		<div class="panel panel-hotel">
+			<div class="panel-heading"><?php echo $texto['hotel']?></div>
+		  	<div class="panel-body">
+				<div class='list-group'>
+					<?php 
+					foreach ($habitaciones as $habitacion) {
+						$id=$habitacion->id_hotel;
+					}?>
+					<?php	$imagenes_habitacion=$this->imagenes_hotel_model->getImagenes($id); ?>
+					<?php foreach ($imagenes_habitacion as $imagenes) { ?>
+		            <div class='col-md-6'>
+		                <a class="fancybox" rel="ligthbox" href="<?php echo base_url().'assets/uploads/hoteles/'.$imagenes->imagen;?>">
+		                    <img class="img-responsive gris" alt="" src="<?php echo base_url().'assets/uploads/hoteles/'.$imagenes->imagen;?>" />
+		                    <div class='text-right'>
+		                        <small class='text-muted'><?php echo $imagenes->descripcion;?></small>
+		                    </div>
+		                </a>
+		            </div> 
+		            <?php } ?>
+				</div>
+			</div>
+			<div class="panel-body">
+					<center>
+						<a href="javascript:window.history.back();" type="submit" class="btn btn-default btn-xlarge" title="<?php $texto['volver']?>" rel="tooltip">
+							<span class="icon-chevron-left"></span>
+						</a>
+					</center>
+			</div>
+		</div>
+	</div>							
+</div>
